8f55bc1317d04afea5ab57d9c2a54927c54267e3
Paresque <thyago.paresque@webiss.com.br>
2021-03-29 23:12:47 -0300

    Adicionando um endpoint limpo porque o health check do load balancer não está funcionando

---
+++ src/SGM.Autenticacao.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SGM.Autenticacao.Api.Controllers
+{
+    [EnableCors("ApiCorsPolicy")]
+    [Route("api/autenticacao/health-check")]
+    [ApiController]
+    public class HealthCheckController : Controller
+    {       
+        [HttpGet]
+        public IActionResult HealthCheck()
+		{
+            return Ok();
+		}
+    }
+}
