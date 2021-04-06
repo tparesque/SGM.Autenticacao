0c31cb02ca5a16477f1a0f886405fd304cce41c7
Paresque <thyago.paresque@webiss.com.br>
2021-04-05 22:33:37 -0300

    Removendo propriedades que não são utilizadas

--- src/SGM.Autenticacao.Domain/Dto/AuthDto.cs
+++ src/SGM.Autenticacao.Domain/Dto/AuthDto.cs
@@ -4,8 +4,5 @@
     {
         public string Email { get; set; }
         public string Senha { get; set; }
-        public string Nome { get; set; }
-        public string Token { get; set; }       
-
     }
 }
